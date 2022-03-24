a720eef9346bc459b40d6675381184a474c30c26
Ryan Anderson <ryan.anderson@buzzuti.com>
2022-03-24 15:52:26 -0500

    formatting.

--- cmd.go
+++ cmd.go
@@ -9,7 +9,6 @@ import (
 var UICmd = &cobra.Command{
 	Use:   "ui",
 	Short: "Launch a web based UI tool that uses the CLI",
-	Long:  `Additional long description here...`,
 	Run: func(cmd *cobra.Command, args []string) {
 		err := Serve(cmd.Root())
 		if err != nil {
